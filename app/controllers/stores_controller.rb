36855187273ec1553e6eeb3414595bb162500c10
317718 <317718@gmail.com>
2020-11-11 11:22:08 +1000

    completed crud for stores

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -1,5 +1,37 @@
 class StoresController < ApplicationController
+    skip_before_action :verify_authenticity_token
+    
+    def index
+        @stores = Store.all
+    end
+
     def new
-        
+        @store = Store.new
+    end
+
+    def create
+        store = Store.new(store_params)
+        store.user_id = current_user.id
+        store.save
+        redirect_to store_path(store.id)
+    end
+
+    def show
+        @store = Store.find(params[:id])
+    end
+     
+    def edit
+        @store = Store.find(params[:id])
+    end
+
+    def destroy
+        Store.destroy(params[:id])
+        redirect_to stores_path
+    end
+
+    private
+
+    def store_params
+        params.require(:store).permit(:name)
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "stores/new", to: "stores#new"
+  post "stores", to: "stores#create"
+  get "stores", to: "stores#index"
+  get "stores/:id", to: "stores#show", as: "store"
+  get "stores/:id/edit", to: "stores#edit", as: "edit"
+  put "stores/:id", to: "stores#update"
+  delete "stores/:id", to: "stores#destroy", as: "destroy_store"
   root to: "pages#index"
 end
